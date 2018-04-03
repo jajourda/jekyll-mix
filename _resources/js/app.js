9554d91bfd188f55d0f4e981d56c8364edc687f7
Joshua Jourdain <yahwehprovides@joshuas-imac.attlocal.net>
2018-04-03 11:53:12 -0400

    add fullcalendar

--- _resources/js/app.js
+++ _resources/js/app.js
@@ -1,7 +1,12 @@
 var $ = require("jquery");
+var fullCalendar = require("fullcalendar");
 
 
-console.log('Hello, World!')
+console.log('Hello, World!');
 
 console.log($('h1'));
+
+console.log(fullCalendar);
+
+
 
